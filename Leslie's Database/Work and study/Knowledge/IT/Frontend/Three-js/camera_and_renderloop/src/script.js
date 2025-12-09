bb04ca977e4dccf3bca3c7158c48a81982431bd8
Leslie Claussen <78480772+IamLeslieClaussen@users.noreply.github.com>
2025-12-09 08:27:44 +0000

    groups

--- Leslie's Database/Work and study/Knowledge/IT/Frontend/Three-js/camera_and_renderloop/src/script.js
+++ Leslie's Database/Work and study/Knowledge/IT/Frontend/Three-js/camera_and_renderloop/src/script.js
@@ -6,15 +6,38 @@ const scene = new THREE.Scene()
 
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "red", wireframe: true})
 
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 )
 
-scene.add(cubeMesh)
+const cubeMesh1 = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
 
+const cubeMesh2 = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+
+const group = new THREE.Group()
+group.add(cubeMesh)
+group.add(cubeMesh1)
+group.add(cubeMesh2)
+
+scene.add(group)
+
+cubeMesh.position.y = 1
+cubeMesh.position.z = -1
+cubeMesh2.position.y = 4
+cubeMesh.position.z = 2
+
+
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
 
 
 //initialize the camera
@@ -39,26 +62,30 @@ const aspectRatio = window.innerWidth / window.innerHeight
 // initialize the renderer
 const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias: true
 })
 
-
+renderer.setSize(window.innerWidth, window.innerHeight)
+const maxPixelRatio = Math.min(window.devicePixelRatio, 2)
+renderer.setPixelRatio(maxPixelRatio)
 
 //instantiate the controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.autoRotate = true
 
-renderer.setSize(window.innerWidth, window.innerHeight)
+
 
 window.addEventListener('resize', () => {
   // update camera
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  
 })
 
 const renderloop = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight)
   controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(renderloop)
