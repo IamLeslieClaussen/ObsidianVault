67b80f93122c5c9dd5503490f13314dec89b9743
Leslie Claussen <78480772+IamLeslieClaussen@users.noreply.github.com>
2025-12-20 17:27:54 +0000

    quiz app

---
+++ Leslie's Database/Work and study/Knowledge/IT/fullstack/projects/quizApp/src/main.ts
@@ -0,0 +1,15 @@
+import './style.css'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const startScreen = document.getElementById('start-screen');
+    const quizScreen = document.getElementById('quiz-screen');
+    const resultScreen = document.getElementById('result-screen');
+    const startButton = document.getElementById('start-btn');
+    const questionText = document.getElementById('question-text');
+    const answersContainer = document.getElementById('answers-container');
+    const currentQuestionSpan = document.getElementById('current-question');
+    
+
+
+})
